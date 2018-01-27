8fd4b5b09776cb70e370a0d11af1e0ee4e22e5fa
Paolo Sarti <paolo.sarti93@gmail.com>
2018-01-27 14:29:40 +0100

    Fixed help, documentation

--- sig2cli/sig2cli.py
+++ sig2cli/sig2cli.py
@@ -10,9 +10,12 @@ def sig2cli(fn):
     """Decorator that transforms the signature of a function to the input of an argument parser.
     It lets you define a function signature instead of writing a lot of verbose argparse code.
     Keyword arguments are the default.
-    It supports short args (the first letter of each argument) only if they are all different.
+    The command line description is taken from the docstring of the method, with the following conventions:
+        - the description is everything that precedes the string "Arg" (so Arguments, Args) that starts the argument list.
+        - each argument should be in the format <argname> : [argtype] <argumentdescription>
+    The type is inferred from default arguments, or taken from the docstring if specified. Allowed types are "str", "int", "float", "bool".
+    It supports short args (the first letter of each argument) only if they are all different and no argument starts with "h" (reserved for help)
     Default bool arguments are registered as flags.
-
 
     The decorated function should then be called with sys.argv[1:] as the argument.
 
@@ -56,7 +59,10 @@ def sig2cli(fn):
         par_type = {k:_types_dict[v] for k,v in par_typename.items() if v in _types_dict and k in non_defaults}
         par_help = dict(re.findall(r'(\w+)\s*\:(?!\n)\s*(.*)', doc))
     # check that all the starting letters are different
-    easy_short_args = len(set(x[0] for x in sig.parameters.keys())) == len(sig.parameters.keys())
+    initials = set(x[0] for x in sig.parameters.keys())
+    easy_short_args = len(initials) == len(sig.parameters.keys())
+    # "h" is reserved for help
+    easy_short_args = easy_short_args and 'h' not in initials
     for nd in non_defaults:
         names = ('-'+nd[0], '--'+nd) if easy_short_args else ('--'+nd,) 
         parser.add_argument(*names, required=True, type=par_type.get(nd, str), help=par_help.get(nd, ''))
@@ -112,14 +118,15 @@ optional arguments:
     #'''
 
     #'''
-    def b(one, two=2, log=False):
+    def hi(name, times=1, enthusiast=False):
+        """Say hi!
+        Arguments
+            name:       person to greet
+            times:      how many times to greet
+            enthusiast: add an exclamation mark at the end
         """
-        one: int. An int
-        two: int. Another integer
-        """
-        if log:
-            print('Logging')
-        print(one + two)
+        for _ in range(times):
+            print('Hi {}{}'.format(name, '!' if enthusiast else ''))
 
-    run(b)
+    run(hi)
     #'''
